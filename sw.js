76c1942f22020e6043144e9e7dec603c8468575b
Frank Salas <frankrogerstg@gmail.com>
2024-11-08 10:21:18 -0500

    WebApp structure

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('v1').then(cache => cache.addAll([
+			'/',
+			'/index.html',
+			'/styles.css',
+			'/validation.js',
+			'/manifest.json',
+		]))
+	)
+	self.skipWaiting()
+})
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request).then(response => response || fetch(event.request))
+	)
+})
+
+self.addEventListener('push', event => {
+	const data = event.data ? event.data.json() : {}
+	self.registration.showNotification(data.title, {
+		body: data.body,
+		icon: '/icon.png',
+	})
+})
+
